1946260b80e17758fe1a9c11bcd0b7b8468c388b
kosiorski-eu <konrad@kosiorski.eu>
2016-11-18 14:49:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+(function($) {
+
+    $(document).ready(function() {
+		
+	
+		var button = $(".button"),
+			menu = $(".menu");
+		
+		button.on("click", function(){
+			
+			menu.fadeToggle();
+		})
+
+    });
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
